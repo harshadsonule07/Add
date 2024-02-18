4b4324f171db6749ecfd80e65595982602a5684c
harshadsonule07 <160404698+harshadsonule07@users.noreply.github.com>
2024-02-18 20:05:32 +0530

    Add files via upload

---
+++ sum.java
@@ -0,0 +1,21 @@
+//program for addition of tw number
+import java.until.scanner;
+public class Sum
+{
+    public static void main(string atgs[])
+    {
+        int x,y,sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enetr the first number:");
+        x = sc.nextInt();
+        System.out.print("Enter the second number:");
+        y = sc.nextInt();
+        sum=x+y;
+        System.out.println("The sum of the two number x & y is: " + sum);
+    }
+    {
+        int sum = a+b;
+        return sum;
+    }
+}
+    
